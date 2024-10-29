31de12ee65c3f57309c4489ddf8d7048ab083980
Thirawoot.P <thirawoot.put@gmail.com>
2024-10-29 10:45:07 +0700

    delete unuse

--- src/utils/redisUtils.ts
+++
@@ -1,9 +0,0 @@
-import redis from "@/config/redis"
-
-export const setex = async (key: string, value: string, expireTimeInSecond: number) => {
-  await redis.setex(key, expireTimeInSecond, value)
-}
-
-export const del = async (...key: Array<string>) => {
-  await redis.del(key)
-}
