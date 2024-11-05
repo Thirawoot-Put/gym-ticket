cbfe73bc51310a6b15012f02814882939374fe09
Thirawoot.P <thirawoot.put@gmail.com>
2024-11-05 17:57:17 +0700

    change directory

--- src/infrastructure/msg-brokers/msg-broker.repository.ts
+++ src/infrastructure/msg-brokers/msg-broker.repository.ts
@@ -1,8 +1,1 @@
-import { Kafka } from "kafkajs";
 
-const kafkaClient = new Kafka({
-  clientId: "ticketing-app",
-  brokers: ['localhost:9192', 'localhost:9193']
-})
-
-// kafka class with static method for connect kafka client
