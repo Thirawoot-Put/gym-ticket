61b5895cc4b1d0c66976e8ce0101786fa07f9d3e
Thirawoot.P <thirawoot.put@gmail.com>
2024-11-06 14:08:00 +0700

    complete config kafka

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -12,3 +12,5 @@ export const REDIS_URL = process.env.REDIS_URL as string;
 export const MONGO_DB_URI = process.env.MONGO_DB_URI as string
 
 export const LOG_LEVEL = process.env.LOG_LEVEL as string
+
+export const KAFKA_CLIENT_ID = process.env.KAFKA_CLIENT_ID as string
--- src/shared/utils/kafka.ts
+++ src/shared/utils/kafka.ts
@@ -1,8 +1,101 @@
-import { Kafka } from "kafkajs";
+import { Kafka, KafkaMessage } from "kafkajs";
+import { KAFKA_CLIENT_ID } from "../config/env";
 
-const kafkaClient = new Kafka({
-  clientId: "ticketing-app",
-  brokers: ['localhost:9192', 'localhost:9193']
-})
 
 // kafka class with static method for connect kafka client
+class KafkaProducer {
+  private kafka = new Kafka({
+    clientId: KAFKA_CLIENT_ID,
+    brokers: ['localhost:9192', 'localhost:9193']
+  })
+
+  public producer = this.kafka.producer()
+
+  async connect() {
+    try {
+      await this.producer.connect()
+      console.info("Kafka producer connecting success")
+    } catch (e) {
+      console.error("Failed to connect kafka producer", e)
+    }
+  }
+
+  async send(topic: string, message: string) {
+    try {
+      await this.producer.send({
+        topic,
+        messages: [
+          {
+            //key: '' // ---> access some key from function
+            value: message
+          }
+        ]
+      })
+    } catch (e: any) {
+      console.error('Failed to write a message', e)
+    }
+  }
+
+  async disconnect() {
+    await this.producer.disconnect()
+  }
+}
+
+class KafkaConsumer {
+  private kafka = new Kafka({
+    clientId: KAFKA_CLIENT_ID,
+    brokers: ['localhost:9192', 'localhost:9193']
+  })
+
+  public consumer = this.kafka.consumer({ groupId: KAFKA_CLIENT_ID })
+
+  async connect() {
+    try {
+      await this.consumer.connect()
+      console.info('Kafka consumer connecting')
+    } catch (e: any) {
+      console.error('Failed to connect kafka consumer', e)
+    }
+  }
+
+  async subscribe(topic: string) {
+    await this.consumer.subscribe({ topic: topic })
+    console.info(`Consumer subscribe ${topic}`)
+  }
+
+  async consume(processingCB: (message: KafkaMessage) => void | Promise<void>, maxRetries = 3) {
+    let retries = 0
+
+    await this.consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log(`Consume message from ${topic} topic`)
+        let success = false
+
+        while (retries < maxRetries && !success) {
+          try {
+            const result = processingCB(message)
+
+            if (result instanceof Promise) {
+              await result
+            }
+
+            success = true
+          } catch (e) {
+            retries++
+            console.error(`Error to process message ${retries} round:`, e)
+
+            if (retries >= maxRetries) {
+              console.error('Send to DLQ(Dead letter queue) consumer')
+              // other error handling
+            }
+          }
+        }
+      }
+    })
+  }
+
+  async disconnect() {
+    await this.consumer.disconnect()
+    console.info('Kafka consumer disconnected')
+  }
+}
